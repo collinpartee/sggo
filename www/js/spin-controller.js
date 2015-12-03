fd23664eff22e5a7aafb870258a22135fc7e8b0a
collinpartee <collin.partee@gmail.com>
2015-12-03 11:47:34 -0500

    added jquery shuffle plugin

--- www/js/spin-controller.js
+++ www/js/spin-controller.js
@@ -101,5 +101,53 @@ console.log($stateParams);
 function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+   // SECOND SPIN STYLE 
     
+    $(function(){
+	
+	// container is the DOM element;
+	// userText is the textbox
+	
+	var container = $("#container")
+		userText = $('#userText'); 
+	
+	// Shuffle the contents of container
+	container.shuffleLetters();
+
+	// Bind events
+	userText.click(function () {
+		
+	  userText.val("");
+	  
+	}).bind('keypress',function(e){
+		
+		if(e.keyCode == 13){
+			
+			// The return key was pressed
+			
+			container.shuffleLetters({
+				"text": userText.val()
+			});
+			
+			userText.val("");
+		}
+
+	}).hide();
+
+	// Leave a 4 second pause
+
+	setTimeout(function(){
+		
+		// Shuffle the container with custom text
+		container.shuffleLetters({
+			"text": "Test it for yourself!"
+		});
+		
+		userText.val("type anything and hit return..").fadeIn();
+		
+	},4000);
+	
+});
+
+
 });
